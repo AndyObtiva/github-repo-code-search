7d9cf9d9b77b24bd68df6e85205209c0c66f61f9
Andy Maleh <andy.am@gmail.com>
2013-11-27 14:31:44 -0500

    Initial commit on github repo code search

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubRepoCodeSearch::Application.config.secret_key_base = '7d0a3960e40cb0cd10ce4a09b613d3493a2533a6b5c23486ea6a0b49496a84f8a230075387a5a48ead0391acfdf5c3126e8222528f54af0b6c9b8fd955d8982a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GithubRepoCodeSearch::Application.config.session_store :cookie_store, key: '_github-repo-code-search_session'
